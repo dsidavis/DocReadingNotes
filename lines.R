b940ee940907e7ee79c2476ec7cf19affb87f85d
Duncan Temple Lang <duncan@r-project.org>
2019-08-07 16:26:03 -0700

    refining this line detecting method. Not perfect, but okay

--- lines.R
+++ lines.R
@@ -1,13 +1,36 @@
 if(FALSE) {
-o = findLineBreaks1(bb1)
-plot(o$dens, type = "b")
-abline(v = sapply(o$starts, min), col = c("red", "green")[o$r$values + 1])
+    o = findLineBreaks1(bb1)
 
-pcols = unlist(mapply(function(x, val) rep(if(val) "green" else "red", length(x)), o$starts, o$r$values))
-points(o$dens$x, o$dens$y, col = pcols)
+    plot(o$dens, type = "b")
+    abline(v = sapply(o$starts, min), col = "green")
+
+    ss = sapply(o$starts, min)
+    ll = split(bb1, cut(bb1$top + bb1$height, ss))
+    ll.txt = sapply(ll, function(x) paste(x$text[order(x$left)], collapse = " "))    
+
+    if(FALSE ) {
+    # No longer appropriate. Was working from the un-subsetted grps.
+    # But idea/concept is fine.        
+      abline(v = sapply(o$starts, min), col = c("red", "green")[o$r$values + 1])
+      pcols = unlist(mapply(function(x, val) rep(if(val) "green" else "red", length(x)), o$starts, o$r$values))
+      points(o$dens$x, o$dens$y, col = pcols)
+    }
 }
 
-findLineBreaks1 = 
+findLineBreaks1 =
+    #
+    # This works reasonably well.
+    # However, it fails on a few lines on the first page of Amada-2013.pdf, specifically
+    # it returns
+    #  "(J.   Arikawa). Corresponding E-mail   addresses: author.   j   arika@med.hokudai.ac.jp Tel.: +81 11 706 6905; ,   yosimatu@med.hokudai.ac.jp fax: +81 11 706 6906. oped spp.   for   detection   of   SEOV   IgG   antibody   in   blood   from   Rattus
+    # which combines pieces from 3 lines at the bottom of the first column and one line in the second column.
+    # This is because the text in the first column at this point is in a smaller font.
+    # If we split by column first, then we would solve this.
+    # But since we also detect the columns by resolving the lines first, we have a circularity
+    # However, we can return to split by lines within the columns so it would be
+    #  lines
+    #  columns
+    #  lines within columns
 function(bbox, bw = 3, minInRun = 3, minDelta = 0)
 {
     # compute the density of the bottom location, but weight the values
@@ -20,16 +43,16 @@ function(bbox, bw = 3, minInRun = 3, minDelta = 0)
     #!!!!XXXX    bad variable d       delta = c(0, diff(d$y))
     delta = c(0, diff(dens$y))
     # 
-    runs = rle(delta > minDelta)
+    runs = rle(delta >= minDelta)
     # Get an group identifier for each observation in dens$x as to which run it is in.
     g = rep(1:length(runs$length), runs$length)
 
     # group the dens$x into the groups and then only keep this for which
     # the change is > minDelta and also that the run has at least minInRun
-    w = runs$values # & runs$length >= minInRun
+    w = runs$values & runs$length >= minInRun
     grps = split(dens$x, g)
-    starts = grps # [w]
-    browser()
+    starts = grps[w]
+#    browser()
     
     delta2 = c(0, diff(delta))
      # And again bad variable x = d$x    in "casual" version.
